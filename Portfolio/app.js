44ec13fe3d2548b9d2c800bce70352e68263969c
ankorss <ak10464@nyu.edu>
2024-12-07 12:47:57 +0400

    а

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -19,6 +19,81 @@ if (ScrollTrigger.isTouch !== 1) {
 		}
 	})
 
+	// Elegant animation for the title
+	ScrollTrigger.create({
+		trigger: '.about-me__title',
+		start: 'top bottom-=100',
+		end: 'top center',
+		onEnter: () => document.querySelector('.about-me__title').classList.add('visible'),
+		onLeaveBack: () => document.querySelector('.about-me__title').classList.remove('visible'),
+		toggleActions: 'play none none reverse'
+	});
+
+	// Animation for decorative squares
+	gsap.from('.decorative-square.red', {
+		scrollTrigger: {
+			trigger: '.image-container',
+			start: 'top bottom-=100',
+			end: 'top center',
+			scrub: 1
+		},
+		x: '0',
+		y: '0',
+		opacity: 0,
+		duration: 1.5,
+		ease: 'power2.out'
+	});
+
+	gsap.from('.decorative-square.dark', {
+		scrollTrigger: {
+			trigger: '.image-container',
+			start: 'top bottom-=100',
+			end: 'top center',
+			scrub: 1
+		},
+		x: '0',
+		y: '0',
+		opacity: 0,
+		duration: 1.5,
+		ease: 'power2.out'
+	});
+
+	const letters = gsap.utils.toArray('.letter-animation');
+	
+	ScrollTrigger.create({
+		trigger: '.about-me',
+		start: 'top center+=100',
+		onEnter: () => {
+			letters.forEach((letter, index) => {
+				gsap.to(letter, {
+					opacity: 1,
+					y: 0,
+					rotationX: 0,
+					duration: 0.8,
+					delay: index * 0.1,
+					ease: 'back.out(1.7)',
+					color: 'var(--color-header)',
+					textShadow: '0 0 0 rgba(244, 239, 236, 0)',
+					onStart: () => letter.style.visibility = 'visible'
+				});
+			});
+		},
+		onLeaveBack: () => {
+			letters.forEach((letter, index) => {
+				gsap.to(letter, {
+					opacity: 0,
+					y: 50,
+					rotationX: -90,
+					duration: 0.5,
+					delay: (letters.length - index) * 0.05,
+					ease: 'power2.in',
+					color: 'transparent',
+					textShadow: '0 0 5px rgba(244, 239, 236, 0.5)'
+				});
+			});
+		}
+	});
+
 	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
 
 	itemsL.forEach(item => {
@@ -47,4 +122,31 @@ if (ScrollTrigger.isTouch !== 1) {
 		})
 	})
 
+	// Анимация для первой части тэглайна
+	gsap.to('.tagline__part-1', {
+		scrollTrigger: {
+			trigger: '.tagline',
+			start: 'top center',
+			onEnter: () => document.querySelector('.tagline__part-1').classList.add('visible')
+		}
+	});
+
+	// Анимация для второй части тэглайна и клякс
+	gsap.to('.tagline__part-2', {
+		scrollTrigger: {
+			trigger: '.tagline',
+			start: 'top center',
+			onEnter: () => {
+				setTimeout(() => {
+					document.querySelector('.tagline__part-2').classList.add('visible');
+					// Добавляем кляксы с небольшой задержкой
+					setTimeout(() => {
+						document.querySelectorAll('.ink-blot').forEach(blot => {
+							blot.classList.add('visible');
+						});
+					}, 200);
+				}, 800)
+			}
+		}
+	});
 }
